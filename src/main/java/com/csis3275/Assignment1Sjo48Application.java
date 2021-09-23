bcba770b855aba6a6be39025c8d2007f93cd4d36
Stephen Joy <stephenjoy98@gmail.com>
2021-09-23 00:42:51 -0700

    Initial commit

---
+++ src/main/java/com/csis3275/Assignment1Sjo48Application.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1Sjo48Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1Sjo48Application.class, args);
+	}
+
+}
